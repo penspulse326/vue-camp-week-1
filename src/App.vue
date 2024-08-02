0de746b0d34036d2fdc2592037e8e5a6fa99b0a5
SHIN <akiyachiumi@gmail.com>
2024-08-02 22:28:19 +0800

    style: improve coding style

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,35 @@ import { ref, onMounted, nextTick, watch } from 'vue';
 import drinkStock from './data.json';
 
 const drinkList = ref<Drink[] | null>(null);
-const editingInput = ref<HTMLInputElement[]>([]);
 const editedItem = ref<EditedItem | null>(null);
+const editingInput = ref<HTMLInputElement[] | null>(null);
 
 function handleEditItem(name: string, index: number) {
-  const newData: EditedItem = {
-    name,
-    index
-  };
-  editedItem.value = newData;
+  editedItem.value = { name, index };
+
+  nextTick(() => {
+    if (!editingInput.value) return;
+    editingInput?.value[0]?.focus();
+  });
 }
 
+/**
+ * drinkList 是非空陣列時才會在 v-for 跑出來並綁定這個方法
+ * 因此 early return 時只要檢查 drinkList.value 是不是為 null
+ */
 function handleSaveItem() {
   if (!editedItem.value || !drinkList.value) return;
 
   const { name, index } = editedItem.value;
 
   drinkList.value[index].name = name;
-  editedItem.value = {
-    name: '',
-    index: -1
-  };
+  editedItem.value = null;
 }
 
 onMounted(() => {
   setTimeout(() => {
     drinkList.value = drinkStock as Drink[];
   }, 1000);
-});
-
-watch(editedItem, () => {
-  nextTick(() => {
-    editingInput.value[0]?.focus();
-  });
 });
 
 interface Drink {
@@ -69,12 +65,11 @@ interface EditedItem {
             <span v-if="index !== editedItem?.index">{{ item.name }}</span>
             <input
               v-else
-              type="text"
-              :key="item.name"
               ref="editingInput"
-              autofocus
+              type="text"
               v-model="editedItem.name"
               @blur="handleSaveItem"
+              :data-index="index"
             />
           </td>
           <td>
@@ -113,6 +108,10 @@ th,
 td {
   padding: 4px 8px;
   min-width: 100px;
+}
+
+th {
+  border-bottom: 1px solid #ccc;
 }
 
 input {
